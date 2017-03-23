9a25b78c9d9602073e26389d4aa777886290ace3
Dmitriy <dm.zelenets@gmail.com>
2017-03-23 18:49:22 +0200

    Create setBits.c

---
+++ setBits.c
@@ -0,0 +1,20 @@
+/*
+** From given 'x' from 'p' position 'n' bits are taken
+** and placed on the left to 'n' rightmost bits of 'y'.
+** Other bits unchanged.
+*/
+
+unsigned int setBits(unsigned int x,    
+                     unsigned int p,    
+                     unsigned int n,    
+                     unsigned int y){   
+    int bitfields = sizeof(int) * 8;
+    if((2*n) > bitfields){
+        return 0;
+    };
+    x<<=(bitfields-p);
+    x>>=(bitfields-n);
+    x<<=n;
+    int bitmask = ((~0 >> (bitfields - n)) << n);
+    return (x|(y & ~bitmask));
+}
