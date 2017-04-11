aeb285399189220406a9a4ae638dd9164fab4afa
Dmitriy <dm.zelenets@gmail.com>
2017-04-11 16:44:14 +0300

    read config file with internet addresses

--- readConfig.c
+++ readConfig.c
@@ -1,8 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
+#define CONFIG_FILE "/path/to/configuration/file/"
+//static const char CONFIG_FILE[] = "/path/to/configuration/file/"
 
-
-int readConfig(char* array[]){
+int readFile(char *array[]){
     FILE *cnfg;
     if((cnfg = fopen(CONFIG_FILE, "r")) == NULL){
         printf("Could not open config file: %s",strerror(errno));
@@ -14,10 +15,20 @@ int readConfig(char* array[]){
           j < ADDR_NAME_LEN &&
           (c = getc(cnfg)) != EOF){
         if(c == '\n'){
-            array[i] = calloc(j+1, sizeof(char));
+            /** if present delete string "www." from the beginning of the address**/
+            if(strstr(buf, "www.") != NULL){
+                int n = 0;
+                while(buf[n+4] != 0){
+                    buf[n] = buf[n+4];
+                    n++;
+                }
+                for(int m = 1; m < 5; m++)
+                    buf[n+4-m] = 0;
+            }
+            address[i] = calloc(j+1, sizeof(char));
             for(int k=0; k < j+1; k++){
-                array[i][k] = buf[k];
-                array[i][k+1] = '\0';
+                address[i][k] = buf[k];
+                address[i][k+1] = '\0';
             };
             i++;
             j=0;
@@ -28,10 +39,12 @@ int readConfig(char* array[]){
         }
         else
         {
+            //ignore characters before "//" and "//" itself
             if( c == 47 ){ // ASCII code of '/'
                 slash_count++;
                 continue;
             }
+            //place in buf characters after "//"
             if(slash_count == 2){
                 buf[j] = (char) c;
                 j++;
@@ -39,5 +52,5 @@ int readConfig(char* array[]){
         }
     }
     fclose(cnfg);
-    return 0;
 }
+
