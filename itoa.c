4c9962e69bc1fed874c8b72a025746e7ad66609a
Dmitriy <dm.zelenets@gmail.com>
2017-05-13 10:19:36 +0300

    Update itoa.c

--- itoa.c
+++ itoa.c
@@ -1,6 +1,8 @@
-void itoa(uint32_t num, char string[10]){
+#define STR_LEN 5
+void itoa(int num, char string[STR_LEN]){
     char x;
-    char tmp[10];
+    char tmp[STR_LEN];
+    memset(tmp, 0, STR_LEN);
     short i = 0;
     do{
         x = num%10;
@@ -10,9 +12,9 @@ void itoa(uint32_t num, char string[10]){
     }
     while((num /= 10) != 0);
     short k = 0;
-    for(short j = 0; j <= 10; j++){
-        if(tmp[10-j]!=0){
-            string[k] = tmp[10-j];
+    for(short j = 0; j <= STR_LEN; j++){
+        if(tmp[STR_LEN-j]!=0){
+            string[k] = tmp[STR_LEN-j];
             ++k;
             string[k] = '\0';
         }
