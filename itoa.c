165fcced64fac335fbaafc97bb159a245867d57a
Dmitriy <dm.zelenets@gmail.com>
2017-05-14 11:04:18 +0300

    Convert decimal numerical value to string equivalent

--- itoa.c
+++ itoa.c
@@ -1,5 +1,5 @@
 #define STR_LEN 5
-void itoa(int num, char string[STR_LEN]){
+static void custom_itoa(int num, char string[STR_LEN]){
     char x;
     char tmp[STR_LEN];
     memset(tmp, 0, STR_LEN);
@@ -10,14 +10,11 @@ void itoa(int num, char string[STR_LEN]){
         ++i;
         tmp[i] = '\0';
     }
-    while((num /= 10) != 0);
+    while(((num /= 10) != 0) && (i < STR_LEN));
     short k = 0;
-    for(short j = 0; j <= STR_LEN; j++){
-        if(tmp[STR_LEN-j]!=0){
-            string[k] = tmp[STR_LEN-j];
-            ++k;
-            string[k] = '\0';
-        }
+    for(; k < i; k++){
+        string[k] = tmp[i-k-1];
     }
+    string[k+1] = tmp[i];
     return;
 }
